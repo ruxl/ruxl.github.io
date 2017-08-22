12c0f6edf5bebf65d0128fb4d81aceb37c884929
ruxl <sonnyjim96@hotmail.co.uk>
2017-08-22 23:23:41 +0100

    fixedd

--- README.txt
+++ README.txt
@@ -1,3 +1,3 @@
-A Pen created at CodePen.io. You can find this one at https://codepen.io/mcdeeda/pen/VpRpww.
+A Pen created at CodePen.io. You can find this one at https://codepen.io/Tbgse/pen/dYaJyJ.
 
- 
+ A simple terminal text effect written in vanilla JS, no dependencies. Just copy the function into your code and have fun.
--- js/index.js
+++ js/index.js
@@ -1,22 +1,51 @@
-$(document).ready(function(){
-  $('#fullpage').fullpage({
-    onLeave: function(index, nextIndex, direction){
-      if(index == 4 && direction == "down"){
-        $('.scroll').animate({
-          'opacity': '0'
-        }, 100);
-      }
-      else{
-        $('.scroll').animate({
-          'opacity': '0'
-          }, 100).delay(500).animate({
-          'opacity': '1'
-        })
-      }
-      
+// function([string1, string2],target id,[color1,color2])    
+ consoleText(['ruxl.tk', 'Nothing here', 'Fuck off.'], 'text',['tomato','rebeccapurple','lightblue']);
+
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ['#fff'];
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+  window.setInterval(function() {
+
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount)
+      window.setTimeout(function() {
+        var usedColor = colors.shift();
+        colors.push(usedColor);
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute('style', 'color:' + colors[0])
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function() {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x;
     }
-  });
-  $('.scroll').on('click', function(){
-    $.fn.fullpage.moveSectionDown();
-  })
-})
+  }, 120)
+  window.setInterval(function() {
+    if (visible === true) {
+      con.className = 'console-underscore hidden'
+      visible = false;
+
+    } else {
+      con.className = 'console-underscore'
+
+      visible = true;
+    }
+  }, 400)
+}
